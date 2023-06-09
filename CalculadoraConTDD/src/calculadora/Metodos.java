7ac4678100ced5e2d246a54c7e053a35b955c401
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2023-06-09 21:45:14 +0200

    Update Metodos.java

--- CalculadoraConTDD/src/calculadora/Metodos.java
+++ CalculadoraConTDD/src/calculadora/Metodos.java
@@ -1,5 +1,10 @@
 package calculadora;
 
 public class Metodos {
-
+  public double sumar(double operando1, double operando2) {
+		double resultado = 0.000;
+		resultado = operando1 + operando2;
+		
+		return resultado;
+	}
 }
