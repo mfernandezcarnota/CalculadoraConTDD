61097bfcc3fae1f870fc3faa44dad0dfbd382dc1
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2023-06-09 21:50:43 +0200

    Update Calculadora.java

--- CalculadoraConTDD/src/calculadora/Calculadora.java
+++ CalculadoraConTDD/src/calculadora/Calculadora.java
@@ -8,5 +8,24 @@ public class Calculadora {
 			int opcion;
 			boolean op = true;
 			while(op) {
+				
+				System.out.println("************************");
+				System.out.println("Menú.");
+				System.out.println("1. Sumar.");
+				System.out.println("2. Restar.");
+				System.out.println("3. Multiplicar.");
+				System.out.println("4. Dividir.");
+				System.out.println("5. Raíz cuadrada.");
+				System.out.println("6. Función exponencial con base e.");
+				System.out.println("7. Salir.");
+				
+				System.out.println("Por favor, seleccione una opción.");
+				opcion = entrada.nextInt();
+				
+				if (opcion == 7) {
+					System.out.println("Saliendo de la calculadora.");
+					break;
+				} else {
+					switch (opcion) {
 
 }
