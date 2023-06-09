f40df3e272945ba2d0c71b3fb9c75eace9bd1567
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2023-06-09 21:50:24 +0200

    Update Calculadora.java

--- CalculadoraConTDD/src/calculadora/Calculadora.java
+++ CalculadoraConTDD/src/calculadora/Calculadora.java
@@ -1,5 +1,12 @@
 package calculadora;
 
 public class Calculadora {
+  
+      Metodos calcu = new Metodos();
+		  try (Scanner entrada = new Scanner(System.in)) {
+			double operando1, operando2, radicando, exponente, resultado;
+			int opcion;
+			boolean op = true;
+			while(op) {
 
 }
