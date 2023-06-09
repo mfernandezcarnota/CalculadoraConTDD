880ea8a130fa4e02ce9a48d07b45693bed0198d0
Miguel <49518849+mfernandezcarnota@users.noreply.github.com>
2023-06-09 21:45:42 +0200

    Update TestCalculadora.java

--- CalculadoraConTDD/src/calculadora/TestCalculadora.java
+++ CalculadoraConTDD/src/calculadora/TestCalculadora.java
@@ -23,5 +23,28 @@ public class TestCalculadora {
 		assertEquals("Resultado de la suma debería ser igual al valor esperado",
 				valor_esperado, resultado, 0.0000);
 	}
+	
+	@Test
+	public void restarDeberiaDevolverElValorEsperadoCuandoLosOperandosSonValidos() {
+		/* El nombre del metodo se divide en un parte con el nombre del metodo,
+		 * que es lo que se espera cuando el test es un exito,
+		 * cuando se debe producir ese exito
+		 */
+		
+		/*Paso 1: crear y configurar los objetos necesarios para ejecutar el test*/
+		double operando_1 = 10.0000;
+		double operando_2 = 5.1234;
+		double valor_esperado = 4.8766;
+		
+		Metodos calcu = new Metodos();
+				
+		/*Ejecutar el metodo testeado*/
+		double resultado = calcu.restar(operando_1, operando_2);
+		System.out.println("El resultado de la prueba restar es: " + resultado +
+				" y el valor esperado es: " + valor_esperado);
+		/*Comprobar que el resultado obtenido es igual al resultado esperado po el test*/
+		assertEquals("Resultado de la resta debería ser igual al valor esperado",
+				valor_esperado, resultado, 0.0000);
+	}
 
 }
